2f74620eca3b8a01578a5f84e55c823d1089cec3
疯小疯 <34209290+fengbaochun@users.noreply.github.com>
2019-10-29 23:17:34 +0800

    整理 button

--- 2-wxPython-button/PythonApplication1/PythonApplication1/PythonApplication1.py
+++ 2-wxPython-button/PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,30 +1,27 @@
-import wx  
-
+import wx  #导入wx模块
 
 class ButtonFrame(wx.Frame): 
     ClickNum = 0  #定义变量
     def __init__(self):  
         wx.Frame.__init__(self, None, -1, 'Button Demo',   
-                size=(300, 300))  
+                size=(300, 200))  #初始化窗口信息
 
-        panel = wx.Panel(self, -1)  
-        self.button = wx.Button(panel, -1, "OFF", pos=(50, 50),size=(50,50))  
-        self.Bind(wx.EVT_BUTTON, self.OnClick, self.button) #将回调函数与按键绑定
-        self.button.SetDefault() 
+        panel = wx.Panel(self, -1)  #创建面板
+        self.button = wx.Button(panel,-1, "OFF", pos=(50, 50),size=(50,30)) #在面板上添加控件
+        self.Bind(wx.EVT_BUTTON, self.OnClick, self.button) #将回调函数与按键事件绑定
 
-    def OnClick(self, event):  
-        self.button.SetLabel("ON") 
+    def OnClick(self, event):  #回调函数事件
+        self.button.SetLabel("ON") #设置
         self.ClickNum+=1
         if self.ClickNum % 2 == 1:  #根据按下次数判断
-            self.button.SetLabel("ON")
-            print(self.ClickNum)
+            self.button.SetLabel("ON")#修改按键的标签
+            print(self.button.GetLabel())#打印信息（返回按键的标签信息）
         else:
             self.button.SetLabel("OFF")
             self.ClickNum = 0
-            print(self.ClickNum)
+            print(self.button.GetLabel())
 
-
-        
+#主函数入口
 if __name__ == '__main__':  
     # 下面是使用wxPython的固定用法
     app = wx.PySimpleApp()  
