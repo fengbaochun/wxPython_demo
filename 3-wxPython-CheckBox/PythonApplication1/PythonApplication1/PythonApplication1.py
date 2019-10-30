0397b1cad73152c986bd72c4be81eb66a1028bc4
疯小疯 <34209290+fengbaochun@users.noreply.github.com>
2019-10-30 22:26:04 +0800

    丰富demo

--- 3-wxPython-CheckBox/PythonApplication1/PythonApplication1/PythonApplication1.py
+++ 3-wxPython-CheckBox/PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,20 +1,19 @@
-#复选框
-import wx  
+import wx  #导入模块
   
 class CheckBoxFrame(wx.Frame):  
     def __init__(self):  
-        wx.Frame.__init__(self, None, -1, 'Checkbox Example',   
-                size=(400, 300))  
+        wx.Frame.__init__(self, None, -1, 'Checkbox Demo',   
+                size=(400, 300))  #初始化窗口信息
         panel = wx.Panel(self, -1) #创建画板，控件容器
         
-        self.box1 = wx.CheckBox(panel, -1, "Checkbox", (50, 50))  #创建控件
-        self.Bind(wx.EVT_CHECKBOX, self.ChoseBox1, self.box1)#绑定事件
+        self.box = wx.CheckBox(panel, -1, "Checkbox", pos=(50, 50),size=(80,20))  #创建控件
+        self.Bind(wx.EVT_CHECKBOX, self.ChoseBox_Event, self.box)#绑定事件
+        self.box.SetValue(False)#设置当前是否被选中
 
-    def ChoseBox1(self,event):#事件回调函数
-        print("被选中")
-
-
-
+    def ChoseBox_Event(self,event):#事件回调函数
+        print(self.box.GetValue())#打印True 证明复选框已经被选中 False 则反之
+        
+#主函数入口
 if __name__ == '__main__':
     # 下面是使用wxPython的固定用法
     app = wx.PySimpleApp()
