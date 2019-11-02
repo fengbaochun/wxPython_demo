5aa5d39ab6a4d4032efc6775aa409226c0122d69
疯小疯 <34209290+fengbaochun@users.noreply.github.com>
2019-11-02 23:01:14 +0800

    修改 界面调用方式

--- 2-wxPython-button/PythonApplication1/PythonApplication1/PythonApplication1.py
+++ 2-wxPython-button/PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,8 +1,13 @@
 import wx  #导入wx模块
 
+APP_TITLE = u'控件事件、鼠标事件、键盘事件、系统事件'
+
 class ButtonFrame(wx.Frame): 
+    '''程序主窗口类，继承自wx.Frame'''
+
     ClickNum = 0  #定义变量
-    def __init__(self):  
+    def __init__(self, parent): 
+        '''构造函数'''
         wx.Frame.__init__(self, None, -1, 'Button Demo',   
                 size=(300, 200))  #初始化窗口信息
 
@@ -21,10 +26,27 @@ class ButtonFrame(wx.Frame):
             self.ClickNum = 0
             print(self.button.GetLabel())
 
-#主函数入口
-if __name__ == '__main__':  
-    # 下面是使用wxPython的固定用法
-    app = wx.PySimpleApp()  
-    frame = ButtonFrame()  
-    frame.Show()  
+
+class mainApp(wx.App):
+    """
+    在OnInit() 里边申请Frame类，这样能保证一定是在app后调用，
+    这个函数是app执行完自己的__init__函数后就会执行
+    """
+    def OnInit(self):
+        self.Frame = ButtonFrame(None)
+        self.Frame.Show()
+        return True
+
+    """
+    在def OnExit(self):这个是窗口关闭后调用的函数，
+    把要释放的非wx资源或者要保存的放到这个函数里，
+    优雅的退出不留遗憾
+    """
+    def OnExit(self):
+       # self.Frame.ExitFrame()
+       print("关闭窗口后调用")
+
+if __name__ == "__main__":
+    app = mainApp()
     app.MainLoop()
+
